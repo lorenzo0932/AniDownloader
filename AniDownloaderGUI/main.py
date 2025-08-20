ef0ee95d5bd6cd6e3e26e3150418cc84fa5c0dac
lorenzo0932 <60674575+lorenzo0932@users.noreply.github.com>
2025-08-20 21:26:44 +0200

    📦 build(project-structure): enable core module imports   - add __init__.py to anidownloader_core to define it as a package   - adjust sys path in main gui script to enable core module imports

--- AniDownloaderGUI/main.py
+++ AniDownloaderGUI/main.py
@@ -1,4 +1,8 @@
 import sys
+from pathlib import Path
+project_root = Path(__file__).parent.parent
+sys.path.append(str(project_root))
+
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import Qt
 from gui.main_window import AniDownloaderGUI
